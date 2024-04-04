0aefeed194a7eabed89feb035edada46b90e0c40
ch <chelghoum.mohammedouassim@gmail.com>
2024-04-04 18:35:38 +0100

    init

--- Migrations/20240404173355_init.Designer.cs
+++ Migrations/20240404173355_init.Designer.cs
@@ -12,7 +12,7 @@ using b8vB6mN3zAe.Database;
 namespace b8vB6mN3zAe.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    [Migration("20240404172230_init")]
+    [Migration("20240404173355_init")]
     partial class init
     {
         /// <inheritdoc />
